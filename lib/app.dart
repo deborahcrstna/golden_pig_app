edda216b6668ca06b8dd0926bc88043beb233f17
deborahcrstna <deborahcrstna@gmail.com>
2023-07-14 16:41:35 -0400

    add tela de onboarding, configurando assets e font

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:golden_pig_app/features/splash/splash_page.dart';
+import 'package:golden_pig_app/features/onboarding/onboarding_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashPage(),
+      home: OnboardingPage(),
     );
   }
 }
--- lib/common/constants/app_text_styles.dart
+++ lib/common/constants/app_text_styles.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class appTextStyles {
   appTextStyles._();
 
-  static const TextStyle bigText = TextStyle(fontSize: 50.0 , fontWeight: FontWeight.w700);
+  static const TextStyle bigText = TextStyle(
+     fontFamily: 'Inter',
+     fontSize: 50.0 ,
+     fontWeight: FontWeight.w700
+     );
 }
